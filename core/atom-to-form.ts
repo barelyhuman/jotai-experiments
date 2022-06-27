4ddb0cb00cbe4c776e08c1e9e89ce0168a21f5fd
Reaper <ahoy@barelyhuman.dev>
2022-06-27 12:08:12 +0530

    create the basic test sandbox

---
+++ core/atom-to-form.ts
@@ -0,0 +1,63 @@
+import { atom } from "jotai";
+
+export const atomsToForm = (atoms, { validate }) => {
+  return atom(
+    (get) => {
+      let valid = true;
+      let dirty = false;
+
+      const values = Object.fromEntries(
+        Object.entries(atoms).map(([k, v]:[k:any,v:any]) => {
+          const val:any = get(v);
+
+          if (val.isDirty) {
+            dirty = true;
+          }
+
+          if (val.error) {
+            valid = false;
+          }
+
+          return [k, val];
+        })
+      );
+
+      // separated for dev reasons
+      const result:any = {
+        errors: [],
+        values,
+        isValid: valid,
+        isDirty: dirty
+      };
+
+      try {
+        const _validation = validate(values);
+        if (_validation instanceof Promise) {
+          result.isValidating = true;
+          _validation
+            .then(() => {
+              result.isValidating = false;
+            })
+            .catch((err) => {
+              result.isValidating = false;
+              result.isValid = false;
+              result.errors.push(err);
+            });
+        }
+      } catch (err) {
+        if (result.isValidating) {
+          result.isValidating = false;
+        }
+        result.isValid = false;
+        result.errors.push(err);
+      }
+
+      return result;
+    },
+    (get, set, arg:any) => {
+      if (atoms[arg.key]) {
+        set(atoms[arg.key], arg.value);
+      }
+    }
+  );
+};
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAtom } from "jotai";
+import { atomWithValidate } from "jotai-form";
+import { atomsToForm } from "jotai-experiments";
 
-function App() {
+const firstNameAtom = atomWithValidate("", {
+  validate: (v) => {
+    if (v.length > 4) return v;
+    else throw new Error("First Name should be greater than 4 character");
+  }
+});
+
+const lastNameAtom = atomWithValidate("", {
+  validate: (v) => {
+    if (v.length > 4) return v;
+    else throw new Error("Last Name should be greater than 4 character");
+  }
+});
+
+const emailAtom = atomWithValidate("", {
+  validate: (v) => {
+    if (v.endsWith("@gmail.com")) {
+      return v;
+    }
+    throw new Error("Invalid email");
+  }
+});
+
+const formAtom = atomsToForm(
+  {
+    email: emailAtom,
+    firstName: firstNameAtom,
+    lastName: lastNameAtom
+  },
+  {
+    validate: async (values:any) => {
+      if (values.firstName.value.length + values.lastName.value.length > 12) {
+        throw new Error("Combined Name length too long");
+      }
+    }
+  }
+);
+
+export default function App() {
+  const [formData, setFormData] = useAtom(formAtom);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      placeholder="email"
+        value={formData.email}
+        onChange={(e) => setFormData({ key: "email", value: e.target.value })}
+      />
+
+      <div>
+        <input
+          placeholder="firstName"
+          value={formData.firstName}
+          onChange={(e) =>
+            setFormData({ key: "firstName", value: e.target.value })
+          }
+        />
+        <input
+          placeholder="lastName"
+          value={formData.lastName}
+          onChange={(e) =>
+            setFormData({ key: "lastName", value: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <ul>
+          {formData.errors?.map((x:any) => (
+            <li>{x.message}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
